ba910f0a83bda244936bbeaad0e9afda4747603d
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-12-04 17:13:16 +0200

    create array of testimonials

---
+++ index.js
@@ -0,0 +1,8 @@
+const testimonial = [
+  {
+    name: "Sheppard U",
+    photoUrl:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    text: "Man, this thing is getting better and better as I learn more about it",
+  },
+];
